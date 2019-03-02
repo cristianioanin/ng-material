ef8040de7734dc440291365e5328bfea148561e5
Cristian Ioanin <cristian.ioanin@gmail.com>
2019-03-02 22:27:47 +0200

    add Angular Material to project, custom color scheme and typography
    get started using Material in Angular: radio, checkbox, select, input, datepicker, buttons, spinners and dialog modules
    use Sass
    set custom themes (color scheme for Angular)
    set custom typography (Google Fonts)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,60 @@
 import { Component } from '@angular/core';
+import { timer } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { EditCourseComponent } from './edit-course/edit-course.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'ng-material';
+	title = 'ng-material';
+	isChecked = true;
+	colors = [ { id: 1, name: 'Red' }, { id: 2, name: 'Green' }, { id: 3, name: 'Blue' } ];
+	preselectedColor = 2;
+	minDate = new Date(2018, 12);
+	maxDate = new Date(2019, 8);
+	categories = [ { name: 'Beginner' }, { name: 'Intermediate' }, { name: 'Advanced' } ];
+	progress = 0;
+	timer;
+	isLoading = false;
+
+	constructor(private dialog: MatDialog) {
+		this.timer = setInterval(() => {
+			this.progress++;
+			if (this.progress === 100) {
+				clearInterval(this.timer);
+			}
+		}, 20);
+
+		this.getCourses().subscribe(() => (this.isLoading = false));
+	}
+
+	onChange($event) {
+		console.log($event);
+	}
+
+	selectCategory(category) {
+		this.categories.filter((c) => c != category).forEach((c) => (c['selected'] = false));
+
+		category.selected = !category.selected;
+	}
+
+	getCourses() {
+		this.isLoading = true;
+		return timer(3000);
+	}
+
+	openDialog() {
+		this.dialog
+			.open(EditCourseComponent, {
+				data:
+					{
+						courseId: 1
+					}
+			})
+			.afterClosed()
+			.subscribe((result) => console.log(result));
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { EditCourseComponent } from './edit-course/edit-course.component';
+import { MatComponentsModule } from './mat-components.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent, EditCourseComponent ],
+	entryComponents: [ EditCourseComponent ],
+	imports: [ BrowserModule, AppRoutingModule, FormsModule, BrowserAnimationsModule, MatComponentsModule ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
